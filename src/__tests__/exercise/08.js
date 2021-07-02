3eec4cbe19887d1ddb55e4f16aa2eac08af6bb04
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-07-02 21:03:01 +0200

    exercise 8 - using a test component

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,35 +2,24 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function UseCounterHookExample(hookProps) {
-  const {count, increment, decrement} = useCounter(hookProps)
-  return (
-    <>
-      <div>Current count: {count}</div>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </>
-  )
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const initialCount = 0
-  const step = 1
-  render(<UseCounterHookExample initialCount={initialCount} step={step} />)
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
 
-  const currentCount = screen.getByText(/current count/i)
-  const incrementButton = screen.getByRole('button', {name: /increment/i})
-  const decrementButton = screen.getByRole('button', {name: /decrement/i})
-
-  expect(currentCount).toHaveTextContent(`Current count: ${initialCount}`)
-  userEvent.click(incrementButton)
-  expect(currentCount).toHaveTextContent(
-    `Current count: ${initialCount + step}`,
-  )
-  userEvent.click(decrementButton)
-  expect(currentCount).toHaveTextContent(`Current count: ${initialCount}`)
+  expect(result.count).toBe(0)
+  act(() => {
+    result.increment()
+  })
+  expect(result.count).toBe(1)
+  act(() => {
+    result.decrement()
+  })
+  expect(result.count).toBe(0)
 })
