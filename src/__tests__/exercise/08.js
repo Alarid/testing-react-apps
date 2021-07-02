417526df19a4afe10ba1cd4d23a76a8bd6434de5
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-07-02 21:14:34 +0200

    exercise 8 - setup function

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,19 +5,43 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let result
+function setup(...args) {
+  const returnVal = {}
   function TestComponent() {
-    result = useCounter()
+    Object.assign(returnVal, useCounter(...args))
     return null
   }
   render(<TestComponent />)
+  return returnVal
+}
 
+test('exposes the count and increment/decrement functions', () => {
+  const result = setup()
   expect(result.count).toBe(0)
   act(() => {
     result.increment()
   })
   expect(result.count).toBe(1)
+  act(() => {
+    result.decrement()
+  })
+  expect(result.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const initialCount = 2
+  const result = setup({initialCount})
+  expect(result.count).toBe(initialCount)
+})
+
+test('allows customization of the step', () => {
+  const step = 2
+  const result = setup({step})
+  expect(result.count).toBe(0)
+  act(() => {
+    result.increment()
+  })
+  expect(result.count).toBe(2)
   act(() => {
     result.decrement()
   })
